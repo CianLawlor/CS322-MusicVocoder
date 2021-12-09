94998911fbb0467b84b0a933082ef95646adb136
CianLawlor <68199389+CianLawlor@users.noreply.github.com>
2021-12-09 15:41:04 +0000

    PitchShifting To Mic

--- src/BlazorWpfApp/MainWindow.xaml.cs
+++ src/BlazorWpfApp/MainWindow.xaml.cs
@@ -7,7 +7,6 @@ using WebviewAppTest;
 using NAudio.Wave.SampleProviders;
 using NAudio.Wave;
 using NAudio.Midi;
-using NAudio.CoreAudioApi;
 using Syncfusion.Blazor;
 using System;
 using System.Collections.Generic;
@@ -27,6 +26,7 @@ namespace BlazorWpfApp
         private string _recordStatus;
         private int _pitchValue = 0;
         private float pitchFactor = 1.0f;
+        private int SampleRate = 48000;
         private SmbPitchShiftingSampleProvider pitch;
         private BufferedWaveProvider bufferedWaveProviderPitch;
         private BufferedWaveProvider bufferedWaveProviderNormal;
@@ -114,22 +114,19 @@ namespace BlazorWpfApp
 
         private void onPitchStatusUpdate(object sender, EventArgs e)
         {
-            System.Diagnostics.Debug.WriteLine(((AppState)sender).recordStatus + " - " + ((AppState)sender).pitchStatus);
+            //System.Diagnostics.Debug.WriteLine(((AppState)sender).recordStatus + " - " + ((AppState)sender).pitchStatus);
             _pitchStatus = ((AppState)sender).pitchStatus;
         }
 
         private void onPitchValueUpdate(object sender, EventArgs e)
         {
             _pitchValue = ((AppState)sender).pitchValue;
-            System.Diagnostics.Debug.WriteLine(_pitchValue);
+            //System.Diagnostics.Debug.WriteLine(_pitchValue);
         }
 
         private void onRecordStatusUpdate(object sender, EventArgs e)
         {
             _recordStatus = ((AppState)sender).recordStatus;
-            // Defines a semitone
-            var semitone = Math.Pow(2, 1.0 / 12);
-            var semitoneCount = Int32.Parse("4");
 
             // Calculates semitones needed in either direction
             var upOneTone = 100.0f;
@@ -154,11 +151,10 @@ namespace BlazorWpfApp
 
                 bufferedWaveProviderPitch = new BufferedWaveProvider(pitchStream.WaveFormat);
                 pitch = new SmbPitchShiftingSampleProvider(bufferedWaveProviderPitch.ToSampleProvider());
-                pitch.PitchFactor = pitchFactor;
 
                 waveOut = new NAudio.Wave.DirectSoundOut();
 
-                if (_pitchStatus == "record" && _pitchValue != -1)
+                if (_pitchStatus == "record")
                 {
                     pitchStream.StartRecording();
                 }
@@ -208,20 +204,32 @@ namespace BlazorWpfApp
             }
         }
 
+        String lastNote = "";
         private double calculateSemitoneDiff(double voiceFreq, string midiFreq)
         {
             // The Initialisation can be Extracted to Run Once at start of program
-            string[] notes = new string[] { "A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#" };
+            string[] notes = new string[] { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };
 
             List<int> noteNumbers = new List<int>();  // Holds all Notes Generated ([A1, A#1, B1 ... F8, F#8, G8])
             List<String> fullNotes = new List<String>();    // Holds Notes MIDI Number (https://newt.phys.unsw.edu.au/jw/graphics/notes.GIF)
 
             // Creates List of All Notes in Each Octave ([A1, A#1, B1 ... F8, F#8, G8]) (https://musicnotes101.files.wordpress.com/2010/04/piano.jpg)
+            
+            
             for (int i = 0; i < 8; i++)
             {
-                for (int j = 0; j < 8; j++)
+                if (i == 0)
                 {
-                    fullNotes.Add(notes[j] + i);
+                    fullNotes.Add("A0");
+                    fullNotes.Add("A#0");
+                    fullNotes.Add("B0");
+                }
+                else
+                {
+                    for (int j = 0; j < 12; j++)
+                    {
+                        fullNotes.Add(notes[j] + i);
+                    }
                 }
             }
             int count = 21;
@@ -232,21 +240,36 @@ namespace BlazorWpfApp
                 noteNumbers.Add(count);
                 count++;
             }
-
+            List<double> midiFreqs = new List<double>() { 27.5, 29.14, 30.87, 32.7, 34.65, 36.71, 38.89, 41.2, 43.65, 46.25, 49, 51.91, 55, 58.27, 61.74, 65.41, 69.3, 73.42, 77.78, 82.41, 87.31, 92.5, 98, 103.83, 110, 116.54, 123.47, 130.81, 138.59, 146.83, 155.56, 164.81, 174.61, 185, 196, 207.65, 220, 233.08, 246.94, 261.63, 277.18, 293.66, 311.13, 329.63, 349.23, 369.99, 392, 415.3, 440, 466.16, 493.88, 523.25, 554.37, 587.33, 622.25, 659.25, 698.46, 739.99, 783.99, 830.61, 880, 932.33, 987.77, 1046.5, 1108.73, 1174.66, 1244.51, 1318.51, 1396.91, 1479.98, 1567.98, 1661.22, 1760, 1864.66, 1975.53, 2093, 2217.46, 2349.32, 2489.02, 2637.02, 2793.83, 2959.96, 3135.96, 3322.44, 3520, 3729.31, 3951.07, 4186.01, 4434.92, 4698.63, 4978.03, 5274.04, 5587.65, 5919.91, 6271.93, 6644.88, 7040, 7458.62, 7902.13};
+            
+            
+            // Calculate MIDI Note Values (48 = A4, 49 = A#4...)
 
             int index = fullNotes.IndexOf(midiFreq);        // Takes MIDI Input (C3) and Gets It's Index
-            //System.Diagnostics.Debug.WriteLine(index);
             int noteID = noteNumbers[index];                // Using Index, Finds MIDI Inputs Corresponding MIDI Value
 
-            double midiFreqNew = Math.Pow(2, (noteID - 49) / 12.0) * 440;   // Calculates MIDI Note Frequency from Value
+            System.Diagnostics.Debug.WriteLine(noteID);
+            //(a / 32) * (2 ** ((note - 9) / 12));
+            double midiFreqNew = Math.Pow(2, ((noteID - 8) / 12.0)) * (440/32);   // Calculates MIDI Note Frequency from Value
 
+            int closestFreq = midiFreqs.IndexOf(midiFreqs.OrderBy((v) => Math.Abs(v - voiceFreq)).First());
+            String newNote = "";
+            try
+            {
+                newNote = fullNotes[closestFreq];
+            } catch(Exception ex)
+            {
+                newNote = lastNote;
+            }
+
+            lastNote = newNote;
+            System.Diagnostics.Debug.WriteLine(newNote);
             // Calculates Difference Between Fundamental Freq. of our Voice and MIDI Note, Then Converts it to Semitone Difference
-            //System.Diagnostics.Debug.WriteLine(Math.Round(Math.Abs(voiceFreq - midiFreqNew) * Math.Pow(2, 1.0 / 12)));
-            return Math.Round(Math.Abs(voiceFreq - midiFreqNew) * Math.Pow(2, 1.0 / 12));
+            System.Diagnostics.Debug.WriteLine(Math.Round(Math.Abs(voiceFreq - midiFreqNew)));
+            return (closestFreq - index);
         }
 
-        double[] lastBuffer;
-
+        float lastPitch = 1.0f;
         private void InjectPitch(object sender, WaveInEventArgs e)
         {
             if (_recordStatus == "stop")
@@ -264,35 +287,45 @@ namespace BlazorWpfApp
             //}
             bufferedWaveProviderPitch.AddSamples(e.Buffer, 0, e.BytesRecorded); //Add the mic audio to the buffer
 
-            int bytesPerSample = sourceStream.WaveFormat.BitsPerSample / 8;
-            int SamplesRecorded = e.BytesRecorded / bytesPerSample;
-            if (lastBuffer is null || lastBuffer.Length != SamplesRecorded)
-                lastBuffer = new double[SamplesRecorded];
-            for (int i = 0; i < SamplesRecorded; i++)
-                lastBuffer[i] = BitConverter.ToInt16(e.Buffer, i * bytesPerSample);
-
-            if (lastBuffer.Length > 0)
+            var floatBuffer = new List<double>();
+            for (int index = 0; index < e.BytesRecorded; index += 2)
             {
-                int sampleRate = 41_000;
+                short sample = BitConverter.ToInt16(e.Buffer, index);
+                double sample32 = (double)sample;
+                sample32 /= (double)Int16.MaxValue;
+                floatBuffer.Add(sample32);
+            }
 
-                // How Many Times the Audio Should Be Measured Each Second
+            if (floatBuffer.Count > 0)
+            {
+            int sampleRate = 48_000;
 
-                double[] window = FftSharp.Window.Hanning(lastBuffer.Length);
-                double[] windowed = FftSharp.Window.Apply(window, lastBuffer);
-                double[] zeroPadded = FftSharp.Pad.ZeroPad(windowed);
-                double[] psd = FftSharp.Transform.FFTpower(zeroPadded);
+            // How Many Times the Audio Should Be Measured Each Second
 
-                double[] freq = FftSharp.Transform.FFTfreq(sampleRate, psd.Length);
+            double[] window = FftSharp.Window.Hanning(floatBuffer.Count);
+            double[] windowed = FftSharp.Window.Apply(window, floatBuffer.ToArray());
+            double[] zeroPadded = FftSharp.Pad.ZeroPad(windowed);
+            double[] psd = FftSharp.Transform.FFTpower(zeroPadded);
 
-                // Get the Fundamental Frequency by Finding Highest Power Index
-                int maxIndex = psd.ToList().IndexOf(psd.Max());         // Finds the Frequency with the highest power
-                double fundamentalFreq = freq[maxIndex];                // Sets 'fundamentalFreq' to that highest frequency
-                System.Diagnostics.Debug.WriteLine("Max Freq Is " + fundamentalFreq);    // Logs our Fundamental Frequency
+            double[] freq = FftSharp.Transform.FFTfreq(sampleRate, psd.Length);
 
-                // Get Difference Between Fundamental Pitch and MIDI Input in Semitones
-                float semitoneDiff = (float)(fundamentalFreq - calculateSemitoneDiff(fundamentalFreq, "C#1"));
-                System.Diagnostics.Debug.WriteLine(semitoneDiff);
-                //pitch.PitchFactor = (float) fundamentalFreq - (float)semitoneDiff;
+            // Get the Fundamental Frequency by Finding Highest Power Index
+            int maxIndex = psd.ToList().IndexOf(psd.Max());         // Finds the Frequency with the highest power
+            double fundamentalFreq = freq[maxIndex];                // Sets 'fundamentalFreq' to that highest frequency
+
+            // Get Difference Between Fundamental Pitch and MIDI Input in Semitones
+            double semitoneDiff = calculateSemitoneDiff(fundamentalFreq, "C4");
+            
+            var diff = Math.Pow(semitoneDiff, 1.0 / 12);
+            if(diff is double.NaN)
+            {
+                    diff = lastPitch;
+            } else
+            {
+                    lastPitch = (float) diff;
+            }
+            System.Diagnostics.Debug.WriteLine("Diff " + diff);
+                pitch.PitchFactor = (float) diff;
             }
         }
 
